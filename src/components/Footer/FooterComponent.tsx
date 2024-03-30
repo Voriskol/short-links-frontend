d88794912bf5e5b23e82b3b86e030188c785114e
Kirill <pesterevkv2003@gmail.com>
2024-03-31 00:40:24 +0300

    upd: mvp header footer components

--- src/components/Footer/FooterComponent.tsx
+++ src/components/Footer/FooterComponent.tsx
@@ -1,30 +1,80 @@
 'use client'
 import styles from "./ComponentFooter.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 
 const FooterComponent = () => {
     return (
         <>
-            <section className={styles.footer}>
+            <div className={styles.footer__topLine}></div>
+            <footer className={styles.footer}>
                 <div className={styles.container}>
                     <div className={styles.footer__flex}>
                         <div className={styles.footer__flexItem}>
-                            <Image src="/VK.svg"
-                                   width={24}
-                                   height={25}
-                                   alt="VKlogo"
-                                   className={styles.VKlogo}/>
-                            <Image src="/Telegram.svg"
-                                   width={24}
-                                   height={25}
-                                   alt="TGlogo"
-                                   className={styles.TGlogo}/>
+                            <div className={styles.logo}>
+                                <Link href={"#"} className={styles.flexItem__link}>
+                                    <Image src="/VK.svg"
+                                           width={40}
+                                           height={40}
+                                           alt="VKlogo"
+                                    />
+                                </Link>
+                            </div>
+                            <div className={styles.logo}>
+                                <Link href={"#"} className={styles.flexItem__link}>
+                                    <Image src="/Telegram.svg"
+                                           width={40}
+                                           height={40}
+                                           alt="TGlogo"
+                                    />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={styles.footer__flexItem}>
+                            <div className={styles.flexItem__details}>
+                                <h2 className={styles.flexItem__title}>
+                                    Сервис
+                                </h2>
+                                <nav className={styles.flexItem__navigation}>
+                                    <div className={styles.flexItem__link}><Link href={"#"}>Начало</Link></div>
+                                    <div className={styles.flexItem__link}><Link href={"#"}>Тарифы</Link></div>
+                                    <div className={styles.flexItem__link}><Link href={"#"}>Ответы на вопросы</Link></div>
+                                    <div className={styles.flexItem__link}><Link href={"#"}>Обратная связь</Link></div>
+                                </nav>
+                            </div>
+                        </div>
+                        <div className={styles.footer__flexItem}>
+                            <div className={styles.flexItem__details}>
+                                <h2 className={styles.flexItem__title}>
+                                    Поддержка
+                                </h2>
+                                <nav className={styles.flexItem__navigation}>
+                                    <div className={styles.flexItem__link}><Link href={"#"}>Описание API</Link></div>
+                                    <div className={styles.flexItem__link}><Link href={"#"}>Пожаловаться</Link></div>
+                                </nav>
+                            </div>
+                        </div>
+                        <div className={styles.footer__flexItem}>
+                            <div className={styles.flexItem__details}>
+                                <h2 className={styles.flexItem__title}>
+                                    Быстрый доступ
+                                </h2>
+                                <nav className={styles.flexItem__navigation}>
+                                    <div className={styles.flexItem__link}>
+                                        <Link href={"#"}>Создать ссылку</Link></div>
+                                    <div className={styles.flexItem__link}>
+                                        <Link href={"#"}>Войти в аккаунт</Link></div>
+                                    <div className={styles.flexItem__link}>
+                                        <Link href={"#"}>Зарегистрироваться</Link></div>
+                                    <div className={styles.flexItem__link}>
+                                        <Link href={"#"}>Статус аккаунта</Link></div>
+                                </nav>
+                            </div>
                         </div>
                     </div>
-
                 </div>
-            </section>
+            </footer>
         </>
     );
 };
--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -3,16 +3,28 @@ import styles from "./ComponentHeader.module.css";
 import Link from "next/link";
 const HeaderComponent = () => {
     return (
-        <header className={styles.header}>
-            <div className={styles.container}>
-                <ul className={styles.NavBar}>
-                    <Link href={"#"}><li>Начало</li></Link>
-                    <Link href={"#"}><li>Тарифы</li></Link>
-                    <Link href={"#"}><li>Ответы на вопросы</li></Link>
-                    <Link href={"#"}><li>Обратная связь</li></Link>
-                </ul>
-            </div>
-        </header>
+
+            <header className={styles.header}>
+                <div className={styles.container}>
+                    <ul className={styles.NavBar}>
+                        <Link href={"#"}>
+                            <li>Начало</li>
+                        </Link>
+                        <Link href={"#"}>
+                            <li>Тарифы</li>
+                        </Link>
+                        <Link href={"#"}>
+                            <li>Ответы на вопросы</li>
+                        </Link>
+                        <Link href={"#"}>
+                            <li>Обратная связь</li>
+                        </Link>
+                    </ul>
+                </div>
+                <div className={styles.header__topLine}></div>
+            </header>
+
+
     );
 };
 
